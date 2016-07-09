909a1a85cd5a47692573599ab28481d4d2821175
merser <soren.merser@gmail.com>
2016-07-09 11:04:58 +0200

    Sections rewritten
    Major revision on section selection now handler using Angular.
    Minor layout changes

--- choices.php
+++ choices.php
@@ -1,1 +1,13 @@
-{ "records":[{"Name":"Interactive Stats", "Link":"index.html"}, {"Name":"Free Software", "Link": "javasta2.html"},{"Name":"Books & Manuals", "Link":"javasta3.html"}, {"Name":"Demos & Tutorials", "Link":"javasta4.html"}, {"Name":"Other Links", "Link":"javasta5.html"}, {"Name":"About this Website", "Link":"javasta6.html"}, {"Name":"What's News", "Link":"javasta7.html"}, {"Name":"John Pezzullo", "Link":"JCPhome.html"}, {"Name":"Statistical Jokes", "Link":"jokes.html"}, {"Name":"Google Site Search", "Link":"GoogleSiteSearch.html"}] }
+{ "records":[
+            {"Name":"Interactive Stats", "Tag":"Index", "Link":"index.html"}, 
+            {"Name":"Free Software", "Tag":"Free", "Link": "javasta2.html"},
+            {"Name":"Books & Manuals", "Tag":"Books", "Link":"javasta3.html"},
+            {"Name":"Demos & Tutorials", "Tag":"Demos", "Link":"javasta4.html"},
+            {"Name":"Other Links", "Tag":"Link", "Link":"javasta5.html"},
+            {"Name":"About this Website", "Tag":"About", "Link":"javasta6.html"},
+            {"Name":"What's New", "Tag":"News", "Link":"javasta7.html"},
+            {"Name":"John Pezzullo", "Tag":"Pezzullo", "Link":"JCPhome.html"},
+            {"Name":"Statistical Jokes", "Tag":"Jokes", "Link":"jokes.html"},
+            {"Name":"Site Search", "Tag":"Search", "Link":"search.html"}
+            ]
+}
