33db977a966a9ce6d1af5a8269edeefd028fa3da
merser <soren.merser@gmail.com>
2016-07-10 11:45:01 +0200

    Added new link: gretl
    Section selection bar color handling changed such that html ankers href
    behaves like buttons

--- choices.php
+++ choices.php
@@ -1,13 +1,13 @@
 { "records":[
-            {"Name":"Interactive Stats", "Tag":"Index", "Link":"index.html"}, 
-            {"Name":"Free Software", "Tag":"Free", "Link": "javasta2.html"},
-            {"Name":"Books & Manuals", "Tag":"Books", "Link":"javasta3.html"},
-            {"Name":"Demos & Tutorials", "Tag":"Demos", "Link":"javasta4.html"},
-            {"Name":"Other Links", "Tag":"Link", "Link":"javasta5.html"},
-            {"Name":"About this Website", "Tag":"About", "Link":"javasta6.html"},
-            {"Name":"What's New", "Tag":"News", "Link":"javasta7.html"},
-            {"Name":"John Pezzullo", "Tag":"Pezzullo", "Link":"JCPhome.html"},
-            {"Name":"Statistical Jokes", "Tag":"Jokes", "Link":"jokes.html"},
-            {"Name":"Site Search", "Tag":"Search", "Link":"search.html"}
+            {"Name":"Interactive Stats",    "Tag":"Index",      "Link":"index.html"}, 
+            {"Name":"Free Software",        "Tag":"Free",       "Link":"javasta2.html"},
+            {"Name":"Books & Manuals",      "Tag":"Books",      "Link":"javasta3.html"},
+            {"Name":"Demos & Tutorials",    "Tag":"Demos",      "Link":"javasta4.html"},
+            {"Name":"Other Links",          "Tag":"Link",       "Link":"javasta5.html"},
+            {"Name":"About this Website",   "Tag":"About",      "Link":"javasta6.html"},
+            {"Name":"What's New",           "Tag":"News",       "Link":"javasta7.html"},
+            {"Name":"John Pezzullo",        "Tag":"Pezzullo",   "Link":"JCPhome.html"},
+            {"Name":"Statistical Jokes",    "Tag":"Jokes",      "Link":"jokes.html"},
+            {"Name":"Site Search",          "Tag":"Search",     "Link":"search.html"}
             ]
 }
